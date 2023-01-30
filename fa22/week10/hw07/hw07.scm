284526ec9479522195d70ab1e1013c2cd007eb23
himalalps <haoyu_t@mail.ustc.edu.cn>
2023-01-30 14:15:51 +0800

    week10-2

--- fa22/week10/hw07/hw07.scm
+++ fa22/week10/hw07/hw07.scm
@@ -1,11 +1,20 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst) 
+    (or (null? asc-lst)
+        (null? (cdr asc-lst)) 
+        (and (ascending? (cdr asc-lst)) 
+            (<= (car asc-lst) (cadr asc-lst)))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+    (if (= base 1) 1
+            (cond
+                ((= exp 0) 1)
+                ((modulo exp 2) (* base (pow base (- exp 1))))
+                (else (square (pow base (/ exp 2)))))))
